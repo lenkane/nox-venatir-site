1595cb0a14c7c75a674a40cfe8ed7b2bd92251de
lenkane <lenkanesc@gmail.com>
2025-09-29 00:39:38 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCWJf_pF47snQ-Wpb1LwpA0NrUJEUSjJiQ",
   authDomain: "nox-venatir.firebaseapp.com",
   projectId: "nox-venatir",
@@ -12,9 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "919967958333",
   appId: "1:919967958333:web:7324699dd96e1fd5c9b154"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
-console.log('Firestore initialized:', db);
