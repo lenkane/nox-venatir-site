901b4940829de4b71409e8b25c48633549267e3b
lenkane <lenkanesc@gmail.com>
2025-09-29 00:08:43 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,10 @@
 // firebase-config.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCWJf_pF47snQ-Wpb1LwpA0NrUJEUSjJiQ",
   authDomain: "nox-venatir.firebaseapp.com",
@@ -7,6 +13,9 @@ const firebaseConfig = {
   messagingSenderId: "919967958333",
   appId: "1:919967958333:web:7324699dd96e1fd5c9b154"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
