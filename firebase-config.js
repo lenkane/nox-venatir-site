12eec22e6330097515c7f6a1135249e080c42600
lenkane <lenkanesc@gmail.com>
2025-10-01 22:53:33 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,5 +5,4 @@ export const firebaseConfig = {
   storageBucket: "nox-venatir.firebasestorage.app",
   messagingSenderId: "919967958333",
   appId: "1:919967958333:web:7324699dd96e1fd5c9b154"
-  import { getStorage } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-storage.js';
 };
